70da62b46b9de623c6f439b540cbd03f3918e15d
Kirill Lukianov <bigfirestart@gmail.com>
2021-11-20 06:19:59 +0300

    feat: full leaderboard

--- junction-2021/Modules/Leaderboard/Cell/LeaderBoardWithActionCell.swift
+++ junction-2021/Modules/Leaderboard/Cell/LeaderBoardWithActionCell.swift
@@ -1,5 +1,5 @@
 //
-//  LeaderBoardTopCell.swift
+//  LeaderBoardWithActionCell.swift
 //  junction-2021
 //
 //  Created by k.lukyanov on 20.11.2021.
@@ -7,21 +7,21 @@
 
 import UIKit
 
-class LeaderBoardCell: UITableViewCell {
-
+class LeaderBoardWithActionCell: UITableViewCell {
 	@IBOutlet weak var wrapper: UIView!
 	@IBOutlet weak var numberLabel: UILabel!
 	@IBOutlet weak var commandNameLabel: UILabel!
 	@IBOutlet weak var pointsLabel: UILabel!
+	@IBOutlet weak var battleButton: UIButton!
 	
 	override func awakeFromNib() {
-        super.awakeFromNib()
-    }
+		super.awakeFromNib()
+	}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
 		setupView()
-    }
+	}
 	
 	private func setupView() {
 		backgroundColor = .appBackground()
@@ -31,13 +31,9 @@ class LeaderBoardCell: UITableViewCell {
 		commandNameLabel.text = "Team name"
 		commandNameLabel.font = .systemFont(ofSize: 15, weight: .semibold)
 
-		pointsLabel.text = "8.5"
+		pointsLabel.text = "12.5"
 		pointsLabel.font = .systemFont(ofSize: 15, weight: .semibold)
 		
 		numberLabel.font = .systemFont(ofSize: 22, weight: .bold)
-	}
-	
-	func asBanner() {
-		wrapper.layer.cornerRadius = 14
 	}
 }
--- junction-2021/Modules/Leaderboard/LeaderboardViewController.swift
+++ junction-2021/Modules/Leaderboard/LeaderboardViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 class LeaderboardViewController: UIViewController {
 	var presenter: LeaderboardPresenterProtocol?
 	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		self.view.backgroundColor = .appBackground()
+	private enum Constants {
+		static let tableCell = String(describing: LeaderBoardWithActionCell.self)
+		static let spaceReuseId = String(describing: SpaceCell.self)
 	}
+	
+	private lazy var tableView: UITableView = {
+		let table = UITableView()
+		table.delegate = self
+		table.dataSource = self
+		table.register(SpaceCell.self, forCellReuseIdentifier: Constants.spaceReuseId)
+		
+		table.register(UINib(resource: R.nib.leaderBoardWithActionCell), forCellReuseIdentifier: Constants.tableCell)
+		return table
+	}()
 	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
@@ -23,5 +33,41 @@ class LeaderboardViewController: UIViewController {
 		DispatchQueue.main.async { [weak self] in
 			self?.navigationController?.navigationBar.sizeToFit()
 		}
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .appBackground()
+		
+		setupTable()
+	}
+	
+	private func setupTable() {
+		view.addSubview(tableView)
+		tableView.backgroundColor = .appBackground()
+		tableView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+		}
+
+		tableView.separatorStyle = .none
+		tableView.contentInset = UIEdgeInsets(top: 45, left: 0, bottom: 0, right: 0)
+	}
+}
+
+extension LeaderboardViewController: UITableViewDelegate {
+	
+}
+
+extension LeaderboardViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		20
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: Constants.tableCell, for: indexPath)
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		1
 	}
 }
